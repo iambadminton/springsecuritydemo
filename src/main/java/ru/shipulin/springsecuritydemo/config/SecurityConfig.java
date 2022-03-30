06f8f939c630cd854e9d023bfba118a9a98d9c94
iambadminton <sfmrs@mail.ru>
2022-03-30 12:49:13 +0300

    Проект изменен в части авторизации - теперь авторизация с помощью Authorities

--- src/main/java/ru/shipulin/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/ru/shipulin/springsecuritydemo/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.shipulin.springsecuritydemo.model.Permission;
 import ru.shipulin.springsecuritydemo.model.Role;
 
 @Configuration
@@ -21,9 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest().authenticated()
                 .and().httpBasic();
 
@@ -35,12 +36,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new InMemoryUserDetailsManager(User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles(Role.ADMIN.name())
+                .authorities(Role.ADMIN.getAuthorities())
                 .build(),
                 User.builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name()).build()
+                        .authorities(Role.USER.getAuthorities())
+                        .build()
         );
     }
 
